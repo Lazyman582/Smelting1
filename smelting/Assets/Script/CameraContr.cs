e784c8ea7b4c1303a9e5b74802b517c8fdd67f51
懒惰者582 <thelazy1008@163.com>
2026-04-15 17:04:21 +0800

    增加了代码

---
+++ smelting/Assets/Script/CameraContr.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+public class CameraContr : MonoBehaviour
+{
+    public bool Istouch = false;
+    public List<GameObject> targetObjects = new List<GameObject>();
+    public Camera cam;
+    public GameObject prefab;
+    public Vector3 spawnPosition;
+    private PlayableDirector playDirector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playDirector = FindObjectOfType<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Touch touch = Input.GetTouch(0);
+            HandleTouchClick(touch);
+
+
+
+        }
+    }
+
+    void HandleTouchClick(Touch touch)
+    {
+
+        Ray ray = cam.ScreenPointToRay(touch.position);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            // 检测是否是目标物体
+            if (targetObjects.Contains(hit.collider.gameObject))
+            {
+                Debug.Log("有效点击：" + hit.collider.name);
+                OnHitTarget(hit.collider.gameObject);
+            }
+        }
+    }
+
+    void OnHitTarget(GameObject obj)
+    {
+        switch (obj.name)
+        {
+            case "ironroe":
+              
+                obj.SetActive(false);
+                GameObject instance = Instantiate(prefab,new Vector3(8.22999954f, -0.939999998f, -7.97100019f), Quaternion.identity);
+                instance.transform.localScale = new Vector3(30,30,30);
+                instance.gameObject.name = "stone";
+                targetObjects.Add(instance);
+                break;
+
+            case "stone":
+                Destroy(obj);
+                playDirector.Play();
+                break;
+            default:
+                
+                break;
+        }
+    }
+
+}
